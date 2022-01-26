a18a1ab081dc66157e3ff86e20c55125af364826
mahmoudabdalkarim94 <m.abdalkareem@foothillsolutions.com>
2022-01-26 21:28:57 +0200

    First commit - Project scaffolding

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifydemo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifydemo1c9aac6b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/App.tsx
@@ -0,0 +1,137 @@
+/* src/App.js */
+import React, {useEffect, useState} from "react";
+import Amplify, {API, graphqlOperation} from "aws-amplify";
+import {createTodo} from "./graphql/mutations";
+import {listTodos} from "./graphql/queries";
+import awsExports from "./aws-exports";
+import {withAuthenticator} from "@aws-amplify/ui-react";
+import {ToDoEntity} from "./types";
+import {AppBar, Button, Chip, Grid, IconButton, TextField, Toolbar,} from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import DoneIcon from "@mui/icons-material/Done";
+
+Amplify.configure(awsExports);
+
+const initialState = { name: "", description: "" };
+
+const App = () => {
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState<ToDoEntity[]>([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  function setInput(key: string, value: string) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData: any = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos");
+    }
+  }
+
+  async function addTodo() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo: ToDoEntity = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    } catch (err) {
+      console.log("error creating todo:", err);
+    }
+  }
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Amplify Crash Course By Mahmoud Abd Al Kareem
+          </Typography>
+          {
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+          }
+        </Toolbar>
+      </AppBar>
+      <Box m={3}>
+        <Grid container>
+          <Grid item xs={3} />
+          <Grid item container spacing={3} xs={6}>
+            <Grid item xs={12}>
+              <TextField
+                label="Name"
+                variant="outlined"
+                value={formState.name}
+                fullWidth
+                onChange={(event) => setInput("name", event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={(event) =>
+                  setInput("description", event.target.value)
+                }
+                value={formState.description}
+                placeholder="Description"
+                label="Description"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" onClick={addTodo} fullWidth>
+                Create Todo
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item xs={3} />
+        </Grid>
+      </Box>
+      <Box m={2}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Already added todos
+        </Typography>
+        {todos.map((todo, index: number) => (
+          <Box key={todo.id} p={0.5} m={2}>
+            <Grid container spacing={2}>
+              <Grid item>
+                {" "}
+                <Chip label={todo?.description} deleteIcon={<DoneIcon />} />
+              </Grid>
+            </Grid>
+          </Box>
+        ))}
+      </Box>
+    </div>
+  );
+};
+
+export default withAuthenticator(App);
